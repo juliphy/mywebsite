463531678ce426d7457e73f001e1f7936df92d25
S. <96592869+juliphy@users.noreply.github.com>
2024-07-30 12:58:49 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+var github_button = document.getElementById("github-button")
+var telegram_button = document.getElementById("telegram-button")
+var discord_button = document.getElementById("discord-button")
+
+discord_button.onclick = function(){window.location.href = "https://discord.com/users/403944094339956747"}
+telegram_button.onclick = function(){window.location.href = "https://t.me/bethatonevalentine"}
+github_button.onclick = function(){window.location.href = "https://github.com/juliphy"}
